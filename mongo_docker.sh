029c31281e6cf1d3e64a384817df3ead703ff0e4
Michael Mell <michael.mell@brightbytes.net>
2018-07-30 12:48:46 -0700

    Persist mongo data locally

--- mongo_docker.sh
+++ mongo_docker.sh
@@ -1,3 +1,7 @@
 # See https://hub.docker.com/_/mongo/
 
-docker run --rm --name jwt_demo_mongo -p 27017:27017 -d mongo:latest
+docker run --rm \
+  --name jwt_demo_mongo \
+  -v `pwd`/mongo_data:/data/db \
+  -p 27017:27017 \
+  -d mongo:latest
